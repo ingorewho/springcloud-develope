3f4313bb386ee35e1c9139dcb6c228504e8dbe31
ignorewho <17761256463@163.com>
2018-11-13 21:29:51 +0800

    提交zuul-filter

---
+++ zuul-server/src/main/java/com/ignore/zuul/filter/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package com.ignore.zuul.filter.config;
+
+import com.ignore.zuul.filter.CommonFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description： TODO
+ * Author: ignore1992
+ * Date: Created in 2018/11/13 21:22
+ * Copyright: Copyright (c) 2018
+ * Version: 0.0.1
+ */
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public CommonFilter commonFilter(){
+        return new CommonFilter();
+    }
+}
