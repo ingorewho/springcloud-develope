b0fafac9c11c0d9f3a3feb60f2da5a3dc17432d6
renzhiqiang-lhq <renzhiqiang-lhq@sinosig.com>
2018-11-22 18:37:04 +0800

    提交druid

---
+++ query-server/src/main/java/com/ignore/QueryServerApplication.java
@@ -0,0 +1,20 @@
+package com.ignore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Author: ignore1992
+ * @Description:
+ * @Date: Created In 16:06 2018/11/22
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.ignore")
+public class QueryServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(QueryServerApplication.class, args);
+    }
+}
