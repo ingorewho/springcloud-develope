45b895fa819807d6f9f01b975ff3d69f1a325baf
renzhiqiang-lhq <renzhiqiang-lhq@sinosig.com>
2019-01-28 17:21:45 +0800

    zuul服务

---
+++ zuul-server/src/main/java/com/ignore/zuul/config/ServerFallback.java
@@ -0,0 +1,60 @@
+package com.ignore.zuul.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class ServerFallback implements FallbackProvider {
+    private final Logger logger = LoggerFactory.getLogger(FallbackProvider.class);
+    @Override
+    public ClientHttpResponse fallbackResponse(Throwable cause) {
+        logger.info("Excption {}",cause);
+        return fallbackResponse();
+    }
+
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+            @Override
+            public void close() {
+                // Do nothing.
+            }
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("sorry please wait a moment.".getBytes());
+            }
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
